0149a248c330bc211940927373902cebc2032c44
Divij002 <divijsaini002@gmail.com>
2024-06-06 00:22:08 +0530

    Checking sorted and rotated arrays

---
+++ Arrays/isSorted.cpp
@@ -0,0 +1,49 @@
+/*Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].*/
+
+#include <iostream>
+#include <climits>
+#include <vector>
+using namespace std;
+
+bool check(vector<int> &nums)
+{
+    int mini = INT_MAX;
+    int prev = INT_MIN;
+    int least;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] < mini)
+        {
+            mini = nums[i];
+            least = i;
+        }
+    }
+
+    int i = least;
+    do
+    {
+        if (nums[i] < prev)
+        {
+            return false;
+        }
+        prev = nums[i];
+
+        i = (i == nums.size() - 1) ? 0 : i + 1;
+
+    } while (i != least);
+
+    return true;
+};
